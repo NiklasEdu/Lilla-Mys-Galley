b6cdcae3c21f38305fd5cf431d41cdc82fec5a50
NiklasEdu <niklas.upperud@edu.newton.se>
2022-09-16 14:21:30 +0200

    Niklas fix 2

--- albumPictures copy 2.js
+++ albumPictures copy 2.js
@@ -20,9 +20,44 @@ document.querySelector('.popup-image span').onclick = () => {
     document.querySelector('.popup-image-title-comment-rating').style.display = 'block';
     document.querySelector('.popup-image').style.display = 'none';
  }
+ document.querySelector('.popup-image-title-comment-rating span').onclick = () => {
+  document.querySelector('.popup-image-title-comment-rating').style.display = 'none';
+  document.querySelector('.popup-image').style.display = 'block';
 
- document.querySelector('.popup-image-title-comment-rating .fa.fa-edit').onclick = () => {
-  document.querySelector('.popup-edit-attribute').style.display = 'block';
-
-  
 }
+
+
+//TITEL POPUP FORM
+ document.querySelector('.popup-image-title-comment-rating #title').onclick = () => {
+  document.querySelector('.popup-edit-title').style.display = 'block';
+}
+
+//CLOSE TITLE
+document.querySelector('.popup-edit-title span').onclick = () => {
+  document.querySelector('.popup-edit-title').style.display = 'none';
+}
+
+//COMMENT POPUP FORM
+
+ document.querySelector('.popup-image-title-comment-rating #comment').onclick = () => {
+  document.querySelector('.popup-edit-comment').style.display = 'block';
+} 
+
+//CLOSE COMMENT
+document.querySelector('.popup-edit-comment span').onclick = () => {
+  document.querySelector('.popup-edit-comment').style.display = 'none';
+}
+
+//RATING POPUP FORM
+
+ document.querySelector('.popup-image-title-comment-rating #rating').onclick = () => {
+  document.querySelector('.popup-edit-rating').style.display = 'block';
+}
+
+//CLOSE RATING
+
+document.querySelector('.popup-edit-rating span').onclick = () => {
+  document.querySelector('.popup-edit-rating').style.display = 'none';
+}
+
+
